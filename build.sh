f8bac01bda015dd6887041701182581063586f12
smford <s.m.ford@gmail.com>
2021-11-01 10:42:46 +0000

    Adding build.sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -eux
+MYAPP="motioneye-snapshotter"
+GOOS=linux GOARCH=amd64 go build -ldflags "-s -w"
+upx ./${MYAPP}
+
+VERSION=$(cat ${MYAPP}.go|grep ^const\ applic|cut -f5 -d\ |sed 's/\"//g')
+
+docker build -t smford/${MYAPP}:${VERSION} -t smford/${MYAPP}:latest .
+docker push smford/${MYAPP}:${VERSION}
+docker push smford/${MYAPP}
